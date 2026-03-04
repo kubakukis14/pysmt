c8c0895a539d687a54b61e0d466755e437745dce
MikAnd <micheli.andrea@gmail.com>
2026-03-04 15:50:44 +0100

    Trying to fix msat on windows

--- pysmt/cmd/installers/msat.py
+++ pysmt/cmd/installers/msat.py
@@ -93,7 +93,7 @@ class MSatInstaller(SolverInstaller):
 
         # First, we need the SWIG-generated wrapper
         for f in os.listdir(sodir):
-            if f.startswith("_mathsat"):
+            if "_mathsat" in f:
                 SolverInstaller.mv(os.path.join(sodir, f), self.bindings_dir)
         SolverInstaller.mv(os.path.join(pdir, "mathsat.py"), self.bindings_dir)
 
