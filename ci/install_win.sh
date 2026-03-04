6240d619d3eba6c304ccc9a58512d3b45ffcb12c
MikAnd <micheli.andrea@gmail.com>
2026-03-04 11:25:28 +0100

    Trying to fix msat on windows

--- ci/install_win.sh
+++ ci/install_win.sh
@@ -14,12 +14,12 @@ python -m pip install --upgrade setuptools
 if [ "${PYSMT_SOLVER}" == "msat" ] || [ "${PYSMT_SOLVER}" == "all" ]
 then
     # VCPKG_INSTALLATION_ROOT is a default env var on GH runners
-    $VCPKG_ROOT = "$env:VCPKG_INSTALLATION_ROOT/installed/x64-windows"
+    VCPKG_ROOT="$VCPKG_INSTALLATION_ROOT/installed/x64-windows"
 
     # Tell the compiler and linker where to look
-    $env:INCLUDE = "$VCPKG_ROOT/include;$env:INCLUDE"
-    $env:LIB = "$VCPKG_ROOT/lib;$env:LIB"
-    $env:PATH = "$VCPKG_ROOT/bin;$env:PATH" # For DLLs at runtime
+    INCLUDE="$VCPKG_ROOT/include;$INCLUDE"
+    LIB="$VCPKG_ROOT/lib;$LIB"
+    PATH="$VCPKG_ROOT/bin;$PATH" # For DLLs at runtime
 fi
 
 # Install the solvers
