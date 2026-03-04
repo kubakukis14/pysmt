56e887baf2c5d0e879bcd2a8f71c4a446352d589
MikAnd <micheli.andrea@gmail.com>
2026-03-04 11:39:54 +0100

    Trying to fix

--- pysmt/cmd/install.py
+++ pysmt/cmd/install.py
@@ -33,7 +33,7 @@ Installer = namedtuple("Installer",
                        ["InstallerClass", "version", "extra_params"])
 INSTALLERS = [
     Installer(MSatInstaller,    "5.6.15", {}),
-    Installer(CVC5Installer,    "1.1.2", {}),
+    Installer(CVC5Installer,    "1.3.3", {}),
     Installer(CVC4Installer,    "1.7-prerelease",
               {"git_version" : "391ab9df6c3fd9a3771864900c1718534c1e4666"}),
     Installer(Z3Installer,      "4.15.0", {}),
