c0f017d6991d424e5076ce971e118e3fece1ec39
MikAnd <micheli.andrea@gmail.com>
2026-03-04 16:36:25 +0100

    Trying to fix msat on windows

--- ci/install_win.sh
+++ ci/install_win.sh
@@ -28,7 +28,7 @@ python install.py --confirm-agreement
 if [ "${PYSMT_SOLVER}" == "msat" ] || [ "${PYSMT_SOLVER}" == "all" ]
 then
     python pysmt/cmd/check_version.py msat
-    dumpbin /dependents "C:\Users\runneradmin\AppData\Roaming\Python\Python313\site-packages\_mathsat.cp313-win_amd64.pyd"
+    dumpbin //dependents "C:\Users\runneradmin\AppData\Roaming\Python\Python313\site-packages\_mathsat.cp313-win_amd64.pyd"
     python -v -c "import mathsat; print(mathsat.__version__)"
 fi
 
